7dfe35526b78d52a72fa9bbb1960e4f9e022a2b0
bvsvntv <developer.basanta@gmail.com>
2026-01-20 11:17:14 +0545

    Parse a print expression

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { readFileSync } from 'node:fs';
 import { Lexer } from './lexer.js';
+import { Parser } from './parser.js';
 
 const source = readFileSync(process.cwd() + '/examples/init.ga', 'utf-8');
 const lexer = new Lexer(source);
 const tokens = lexer.readTokens();
 
-console.log(tokens);
+const parser = new Parser(tokens);
+const stmts = parser.parse();
+
+console.table(stmts);
